ed6270c4342b331470109191dac24b935694f8b9
Ben White <bwhite2140@outlook.com>
2020-08-10 15:15:49 -0700

    add books create route

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -3,28 +3,53 @@ using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Library.Controllers
 {
+  [Authorize]
   public class BooksController : Controller
   {
     private readonly LibraryContext _db;
-    public BooksController(LibraryContext db)
+    private readonly UserManager<ApplicationUser> _userManager;
+    public BooksController(UserManager<ApplicationUser> userManager, LibraryContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      return View(_db.Books.ToList());
+      var userId = this._userManager.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userBooks = _db.Books.Where(entry => entry.User.Id == currentUser.Id);
+      return View(userBooks);
     }
     public ActionResult Create()
     {
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
       return View();
     }
     [HttpPost]
-    public ActionResult Create()
+    public ActionResult Create(Book book, int authorId)
     {
+      var userId = this._userManager.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      book.User = currentUser;
+      _db.Books.Add(book);
+      if (authorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() { AuthorId = authorId, BookId = book.BookId });
+      }
+      _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      return View(thisBook);
     }
   }
 }
