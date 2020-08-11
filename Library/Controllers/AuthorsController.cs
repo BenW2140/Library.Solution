8dc7e08540f7bb7d0eb23f687ce19e0501e511cf
Ben White <bwhite2140@outlook.com>
2020-08-11 13:08:37 -0700

    add authors create route

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -8,6 +8,25 @@ namespace Library.Controller
 {
   public class AuthorsController : Controller
   {
-    
+    private readonly LibraryContext _db;
+    public AuthorsController(LibraryContext _db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Authors.ToList());
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Author author)
+    {
+      _db.Authors.Add(author);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
